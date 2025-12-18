1c763430de3b596f414e9c80116f18131e292ab7
bala-amypo <bala@amypo.in>
2025-12-18 17:43:13 +0000

    User commit

--- src/main/java/com/example/demo/model/PersonProfile.java
+++ src/main/java/com/example/demo/model/PersonProfile.java
@@ -7,5 +7,9 @@ public class PersonProfile{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String personType;
-    private String referenceId
+    private String referenceId;
+    private String fullName;
+    private String department;
+    private Boolean relationshipDeclared;
+    private LocalDateTime createdAt;
 }
