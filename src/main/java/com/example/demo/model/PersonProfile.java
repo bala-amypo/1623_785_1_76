9394c63b4f4c958abef7799acf0aa0cade9630c8
bala-amypo <bala@amypo.in>
2025-12-18 17:52:13 +0000

    User commit

--- src/main/java/com/example/demo/model/PersonProfile.java
+++ src/main/java/com/example/demo/model/PersonProfile.java
@@ -17,4 +17,14 @@ public class PersonProfile{
     private String department;
     private Boolean relationshipDeclared;
     private LocalDateTime createdAt;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+        public String getPersonType(){
+            
+        }
+    }
 }
