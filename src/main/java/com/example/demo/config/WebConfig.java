13bebf02ba00ada74d9bfb110fbf6fd0a236952b
bala-amypo <bala@amypo.in>
2025-12-26 16:29:52 +0000

    User commit

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -21,26 +21,3 @@ public class WebConfig {
         };
     }
 }
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://9148.pro604cr.amypo.ai") // your portal frontend
-                        .allowedMethods("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
