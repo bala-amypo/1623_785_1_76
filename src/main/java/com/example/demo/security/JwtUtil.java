99622c3c56b91b8415dba16e557474a1c4ce3cf6
bala-amypo <bala@amypo.in>
2025-12-28 15:57:29 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -3,23 +3,19 @@ package com.example.demo.security;
 import io.jsonwebtoken.*;
 import org.springframework.stereotype.Component;
 import java.util.Date;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "myverylongsecretkeythatisatleast32chars!";
+    private final String SECRET_KEY = "my-secret-key";
     private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // auto-secure key
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, key)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 
