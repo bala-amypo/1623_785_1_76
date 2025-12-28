4940bc5b6f8e2fa3eab32850b558c40a484e70d1
bala-amypo <bala@amypo.in>
2025-12-28 20:03:53 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,61 +1,61 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import com.example.demo.security.JwtFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import com.example.demo.security.JwtFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
 
-    private final JwtFilter jwtFilter;
+//     private final JwtFilter jwtFilter;
 
-    public SecurityConfig(JwtFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
+//     public SecurityConfig(JwtFilter jwtFilter) {
+//         this.jwtFilter = jwtFilter;
+//     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
+//         http
             
-            .csrf(csrf -> csrf.disable())
+//             .csrf(csrf -> csrf.disable())
 
         
-            .sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
+//             .sessionManagement(session ->
+//                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//             )
 
     
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                        "/auth/**",
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html"
-                ).permitAll()
-                .anyRequest().authenticated()
-            )
+//             .authorizeHttpRequests(auth -> auth
+//                 .requestMatchers(
+//                         "/auth/**",
+//                         "/v3/api-docs/**",
+//                         "/swagger-ui/**",
+//                         "/swagger-ui.html"
+//                 ).permitAll()
+//                 .anyRequest().authenticated()
+//             )
 
             
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
   
 
  
-        return http.build();
-    }
+//         return http.build();
+//     }
 
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-}
+//     @Bean
+//     public AuthenticationManager authenticationManager(
+//             AuthenticationConfiguration config) throws Exception {
+//         return config.getAuthenticationManager();
+//     }
+// }
--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -18,45 +18,45 @@
 //                 ));
 //         }
 // }
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.Components;
+// import io.swagger.v3.oas.models.servers.Server;
+// import io.swagger.v3.oas.models.security.SecurityScheme;
+// import io.swagger.v3.oas.models.security.SecurityRequirement;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+// import java.util.List;
 
-@Configuration
-public class SwaggerConfig {
+// @Configuration
+// public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-
-        
-        SecurityScheme securityScheme = new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER);
+//     @Bean
+//     public OpenAPI customOpenAPI() {
 
         
-        SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("Bearer Authentication");
+//         SecurityScheme securityScheme = new SecurityScheme()
+//                 .name("Authorization")
+//                 .type(SecurityScheme.Type.HTTP)
+//                 .scheme("bearer")
+//                 .bearerFormat("JWT")
+//                 .in(SecurityScheme.In.HEADER);
 
-        return new OpenAPI()
+        
+//         SecurityRequirement securityRequirement = new SecurityRequirement()
+//                 .addList("Bearer Authentication");
+
+//         return new OpenAPI()
                 
-                .servers(List.of(
-                        new Server().url("https://9148.pro604cr.amypo.ai/")
-                ))
+//                 .servers(List.of(
+//                         new Server().url("https://9148.pro604cr.amypo.ai/")
+//                 ))
                 
-                .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication", securityScheme)
-                )
-                .addSecurityItem(securityRequirement);
-    }
-}
+//                 .components(new Components()
+//                         .addSecuritySchemes("Bearer Authentication", securityScheme)
+//                 )
+//                 .addSecurityItem(securityRequirement);
+//     }
+// }
