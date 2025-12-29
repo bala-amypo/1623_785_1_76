5e6625afbbe88ad7bcf92a92ab5b6f693fc24627
bala-amypo <bala@amypo.in>
2025-12-29 02:12:16 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtFilter.java
+++ src/main/java/com/example/demo/security/JwtFilter.java
@@ -1,56 +1,56 @@
-// package com.example.demo.security;
+package com.example.demo.security;
 
-// import jakarta.servlet.FilterChain;
-// import jakarta.servlet.ServletException;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import org.springframework.security.core.context.SecurityContextHolder;
-// import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-// import org.springframework.stereotype.Component;
-// import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-// import java.io.IOException;
-// import java.util.Collections;
+import java.io.IOException;
+import java.util.Collections;
 
-// @Component
-// public class JwtFilter extends OncePerRequestFilter {
+@Component
+public class JwtFilter extends OncePerRequestFilter {
 
-//     private final JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
 
-//     public JwtFilter(JwtUtil jwtUtil) {
-//         this.jwtUtil = jwtUtil;
-//     }
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
-//     @Override
-//     protected void doFilterInternal(HttpServletRequest request,
-//                                     HttpServletResponse response,
-//                                     FilterChain filterChain) throws ServletException, IOException {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
-//         String authHeader = request.getHeader("Authorization");
+        String authHeader = request.getHeader("Authorization");
         
 
-//         if (authHeader != null && authHeader.startsWith("Bearer ")) {
-//             String token = authHeader.substring(7); 
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7); 
 
-//             if (jwtUtil.validateToken(token)) {
-//                 String username = jwtUtil.extractUsername(token);
+            if (jwtUtil.validateToken(token)) {
+                String username = jwtUtil.extractUsername(token);
 
-//                 UsernamePasswordAuthenticationToken authentication =
-//                         new UsernamePasswordAuthenticationToken(
-//                                 username,
-//                                 null,
-//                                 Collections.emptyList() 
-//                         );
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                username,
+                                null,
+                                Collections.emptyList() 
+                        );
 
-//                 authentication.setDetails(
-//                         new WebAuthenticationDetailsSource().buildDetails(request)
-//                 );
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
 
-//                 SecurityContextHolder.getContext().setAuthentication(authentication);
-//             }
-//         }
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
 
-//         filterChain.doFilter(request, response);
-//     }
-// }
+        filterChain.doFilter(request, response);
+    }
+}
